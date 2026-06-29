db0539a3e67d951ac171a5fb8b9c9b5356a648f7
adamkhamisofficiel01-pixel <adamkhamisofficiel01@gmail.com>
2026-06-29 18:02:39 +0100

    scfqsf

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    debug = os.environ.get("FLASK_ENV", "production") == "development"
-    app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+#     debug = os.environ.get("FLASK_ENV", "production") == "development"
+#     app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
